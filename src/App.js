6bff32071384ba50318fdeff2c8b7574cd2a9ba1
SANJAY0115 <sanjaysanju0115@gmail.com>
2023-03-24 19:46:02 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { ProductComponent } from './Components/ProductComponent';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+
+    <ProductComponent/>
+
+    </div>
+
+  );
+}
+
+export default App;
